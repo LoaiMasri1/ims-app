390cb727bf4475bf67eb8a61b5ff2006cc20aee1
hmoodbas <hmoudahamood@gmail.com>
2022-04-11 17:31:27 +0300

    Update on many to many relation

--- ims-api/src/config/database.ts
+++ ims-api/src/config/database.ts
@@ -3,6 +3,9 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Department } from "../entities/department.entity";
 import { Room } from "../entities/room.entity";
+import { Item } from "../entities/item.entity";
+import { Category } from "../entities/category.entity";
+import { ItemCategory } from "../entities/itemcategory.entity";
 
 export const Database = new DataSource({
   type: "mysql",
@@ -13,7 +16,7 @@ export const Database = new DataSource({
   database: "ims",
   synchronize: true,
   logging: false,
-  entities: [User ,Department,Room],
+  entities: [User ,Department,Room, Item, Category, ItemCategory],
   migrations: [],
   subscribers: [],
 });
