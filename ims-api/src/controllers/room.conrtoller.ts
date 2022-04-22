7103b172f5450ed786ef74febedd9e7f294d8951
muthana-abo-alez1 <92632582+muthana-abo-alez1@users.noreply.github.com>
2022-04-23 02:06:55 +0300

    add more specific routes for room entity

--- ims-api/src/controllers/room.conrtoller.ts
+++ ims-api/src/controllers/room.conrtoller.ts
@@ -1,17 +1,64 @@
 import { Room } from "../entities/room.entity";
+import { User } from "./../entities/user.entity";
+import { Department } from "../entities/department.entity";
 import { Request, Response } from "express";
 
+export const createRoom = async (req: Request, res: Response) => {
+    const {type,departmentId,userId} = req.body;
+    const department = await Department.findOne({ where:{id:departmentId} });
+    if (!department) {
+      return res.status(400).json({
+        message: `Department with id ${departmentId} Not Found`,
+      });
+    }
+    const user = await User.findOne({ where:{id:userId} });
+    if (!user) {
+      return res.status(400).json({
+        message: `User with id ${userId} Not Found`,
+      });
+    }
+    try {
+      const room = new Room();
+      room.type=type;
+      room.department=department;
+      room.user=user;
+      room.save();
+        res.status(201).json({
+          message: "Room create successfully",
+        });
+      } catch (error) {
+        res.status(500).json({
+            message: "Room creation failed",
+            err: error,
+          });
+      }
+}
 export const updateRoom = async (req: Request, res: Response) => {
-    const {id,type} = req.body;
-    const exist = await Room.findOne({ where:{id} });
-    if (!exist) {
+    const {id}=req.params as any;
+    const {type,departmentId,userId} = req.body;
+    const room = await Room.findOne({ where:{id} });
+    if (!room) {
       return res.status(400).json({
         message: `Room with id ${id} Not Found`,
       });
     }
+    const department = await Department.findOne({ where:{id:departmentId} });
+    if (!department) {
+      return res.status(400).json({
+        message: `Department with id ${departmentId} Not Found`,
+      });
+    }
+    const user = await User.findOne({ where:{id:userId} });
+    if (!user) {
+      return res.status(400).json({
+        message: `User with id ${userId} Not Found`,
+      });
+    }
     try {
-      exist.type=type;
-      exist.save();
+      room.type=type;
+      room.department=department;
+      room.user=user;
+      room.save();
         res.status(201).json({
           message: "Room update successfully",
         });
@@ -23,8 +70,8 @@ export const updateRoom = async (req: Request, res: Response) => {
       }
 }
 export const searchbyType = async (req: Request, res: Response) => {
-    const {type} = req.query as any;
-    const room = await Room.findOne({ where: { type } });
+    const {type} = req.params;
+    const room = await Room.find({ where: { type } });
     if (!room) {
       return res.status(400).json({
         message: `it is ${type} not found`,
@@ -42,7 +89,7 @@ export const searchbyType = async (req: Request, res: Response) => {
       }
   }
   export const searchbyId = async (req: Request, res: Response) => {
-    const {id} = req.query as any;
+    const {id} = req.params as any;
     const room = await Room.findOne({ where: { id } });
     if (!room) {
       return res.status(400).json({
@@ -60,4 +107,145 @@ export const searchbyType = async (req: Request, res: Response) => {
           });
       }
   }
+
+  export const getAllRoom = async (req: Request, res: Response) => {
+    const room = await Room.find();
+    if (!room) {
+      return res.status(400).json({
+        message: `Room not found`,
+      });
+    }
+    try {
+        res.status(201).json({
+          room
+        });
+      } catch (error) {
+        res.status(500).json({
+            message: "Room search failed",
+            err: error,
+          });
+      }
+  }
+ 
+  export const updateRoombyemployee = async (req: Request, res: Response) => {
+    const {id,userId}=req.params as any;
+    const room = await Room.findOne({ where:{id} });
+    if (!room) {
+      return res.status(400).json({
+        message: `Room with id ${id} Not Found`,
+      });
+    }
+    const user = await User.findOne({ where:{id:userId} });
+    if (!user) {
+      return res.status(400).json({
+        message: `User with id ${userId} Not Found`,
+      });
+    }
+    try {
+      room.user=user;
+      room.save();
+        res.status(201).json({
+          message: "Room update successfully",
+        });
+      } catch (error) {
+        res.status(500).json({
+            message: "Room update failed",
+            err: error,
+          });
+      }
+}
+
+export const DeleteRoombyemployee = async (req: Request, res: Response) => {
+  const {id}=req.params as any;
+  const room = await Room.findOne({ where:{id} }) as any;
+  if (!room) {
+    return res.status(400).json({
+      message: `Room with id ${id} Not Found`,
+    });
+  }
+  try {
+    room.user=null;
+    room.save();
+      res.status(201).json({
+        message: "user remove successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+          message: "user remove failed",
+          err: error,
+        });
+    }
+}
+
+export const updateRoombydepartment = async (req: Request, res: Response) => {
+  const {id,departmentId}=req.params as any;
+  const room = await Room.findOne({ where:{id} });
+  if (!room) {
+    return res.status(400).json({
+      message: `Room with id ${id} Not Found`,
+    });
+  }
+  const department = await Department.findOne({ where:{id:departmentId} });
+  if (!department) {
+    return res.status(400).json({
+      message: `User with id ${departmentId} Not Found`,
+    });
+  }
+  try {
+    room.department=department;
+    room.save();
+      res.status(201).json({
+        message: "Room update successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+          message: "Room update failed",
+          err: error,
+        });
+    }
+}
+
+export const DeleteRoombydepartment = async (req: Request, res: Response) => {
+  const {id}=req.params as any;
+  const room = await Room.findOne({ where:{id} }) as any;
+  if (!room) {
+    return res.status(400).json({
+      message: `Room with id ${id} Not Found`,
+    });
+  }
+  try {
+    room.department=null;
+    room.save();
+      res.status(201).json({
+        message: "department remove successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+          message: "department remove failed",
+          err: error,
+        });
+    }
+}
+
+
+export const DeleteRoom = async (req: Request, res: Response) => {
+  const {id}=req.params as any;
+  const room = await Room.findOne({ where:{id} }) as any;
+  if (!room) {
+    return res.status(400).json({
+      message: `Room with id ${id} Not Found`,
+    });
+  }
+  try {
+    room.remove();
+      res.status(201).json({
+        message: "Room remove successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+          message: "Room remove failed",
+          err: error,
+        });
+    }
+}
   
