952e72b8f01cb31dddeb18c8ceff067ad14f1af3
hmoodbas <hmoudahamood@gmail.com>
2022-05-05 18:03:34 +0300

    Add User Interface

--- ims-webapp/scripts/settings/settings.js
+++ ims-webapp/scripts/settings/settings.js
@@ -1,3 +1,6 @@
-export const BASE_URL = "http://localhost:5000/api/v1/";
+export const BASE_URL = "http://localhost:3000/api/v1/";
 export const DEPARTMENT_URL = BASE_URL + "department";
+const AUTH_URL = "http://localhost:3000/auth/v1/"
+export const REGISTER_URL = AUTH_URL + "register";
+export const USER_URL = BASE_URL + "user";
 export const DELAY = 3000;
--- ims-webapp/scripts/utils/utils.js
+++ ims-webapp/scripts/utils/utils.js
@@ -1,9 +1,27 @@
 import { DEPARTMENT_URL } from "../settings/settings.js";
+import { USER_URL } from "../settings/settings.js";
 
 export function deleteDepartment(id) {
   if (confirm("Are you sure you want to delete this department?")) {
     $.ajax({
       url: `${DEPARTMENT_URL}/${id}`,
+      method: "DELETE",
+      success: function (data) {
+        alert(data.message);
+        location.reload();
+      },
+      error: function (data) {
+        alert(data.responseJSON.message);
+        console.error(data.responseJSON.message);
+      },
+    });
+  }
+}
+
+export function deleteUser(id) {
+  if (confirm("Are you sure you want to delete this user?")) {
+    $.ajax({
+      url: `${USER_URL}/${id}`,
       method: "DELETE",
       success: function (data) {
         alert(data.message);
@@ -30,6 +48,30 @@ export function editDepartment(id) {
       $("#edit-form").find("#id").val(department.id);
       $("#edit-form").find("#name").val(department.name);
       $("#edit-form").find("#floor").val(department.floorNumber);
+      $("#editModal").modal("show");
+    },
+    error: function (data) {
+      alert(data.responseJSON.message);
+      console.error(data.responseJSON.message);
+    },
+  });
+}
+
+export function editUser(id) {
+  $("#edit-form")[0].reset();
+  console.log(id);
+  $.ajax({
+    url: `${USER_URL}/${id}`,
+    method: "GET",
+    dataType: "json",
+    success: function (data) {
+      let { user } = data;
+      $("#edit-form")[0].reset();
+      $("#edit-form").find("#id").val(user.id);
+      $("#edit-form").find("#userName").val(user.username);
+      $("#edit-form").find("#email").val(user.email);
+      $("#edit-form").find("#phone").val(user.phone);
+      $("#edit-form").find("#departmentId").val(user.department.id);
       $("#editModal").modal("show");
     },
     error: function (data) {
