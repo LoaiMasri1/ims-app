67aaa40768d6c65b5ff7733d163081623649d3ad
lefdawyy <92638982+lefdawyy@users.noreply.github.com>
2022-05-10 18:24:07 +0300

    Updates item interface

--- ims-webapp/scripts/utils/utils.js
+++ ims-webapp/scripts/utils/utils.js
@@ -57,51 +57,6 @@ export function editCategory(id) {
   });
 }
 
-export function deleteItem(id) {
-  if (confirm("Are you sure you want to delete this item?")) {
-    $.ajax({
-      url: `${ITEM_URL}/${id}`,
-      method: "DELETE",
-      beforeSend: function (xhr) {
-        xhr.setRequestHeader("authorization", "Bearer " + token);
-      },
-      success: function (data) {
-        alert(data.message);
-        location.reload();
-      },
-      error: function (data) {
-        alert(data.responseJSON.message);
-        console.error(data.responseJSON.message);
-      },
-    });
-  }
-}
-
-export function editItem(id) {
-  $("#edit-form")[0].reset();
-
-  $.ajax({
-    url: `${ITEM_URL}/${id}`,
-    method: "GET",
-    dataType: "json",
-    beforeSend: function (xhr) {
-      xhr.setRequestHeader("authorization", "Bearer " + token);
-    },
-    success: function (data) {
-      let { item } = data;
-      $("#edit-form")[0].reset();
-      $("#edit-form").find("#id").val(item.id);
-      $("#edit-form").find("#name").val(item.name);
-      $("#edit-form").find("#categoryId").val(item.category);
-      $("#editModal").modal("show");
-    },
-    error: function (data) {
-      alert(data.responseJSON.message);
-      console.error(data.responseJSON.message);
-    },
-  });
-}
-
 export function deleteUser(id) {
   if (confirm("Are you sure you want to delete this user?")) {
     $.ajax({
@@ -182,6 +137,28 @@ export function getDepartments() {
     success: function (data) {
       const { department } = data;
       return department;
+    },
+    error: function (data) {
+      Swal.fire({
+        title: "Error",
+        text: data.responseJSON.message,
+        icon: "error",
+      });
+    },
+  });
+}
+
+export function getCategorys() {
+  return $.ajax({
+    url: `${CATEGORY_URL}`,
+    method: "GET",
+    dataType: "json",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("authorization", "Bearer " + token);
+    },
+    success: function (data) {
+      const { category } = data;
+      return category;
     },
     error: function (data) {
       Swal.fire({
